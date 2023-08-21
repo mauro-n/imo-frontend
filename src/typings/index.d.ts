88b2c435ed59cbbf078b1e7c2ee89b79c090ce66
test <mauro.henrique613@gmail.com>
2023-08-21 00:10:11 -0300

    Added typing for app context

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -9,4 +9,33 @@ namespace App {
         categorias: any[],
         tipos: any[]
     }
+
+    type app = {
+        searchResults: App.house[]
+    }
+
+    type AppContext = {
+        app: App.app,
+        setApp: React.Dispatch<SetStateAction<App.app>> | undefined
+    }
+
+    type house = {
+        address: string,
+        areaext: boolean,
+        arealzr: boolean,
+        banheiros: string,
+        codigo: string,
+        data: string,
+        descricao: string,
+        imagens: string[],
+        lat: string,
+        log: string,
+        metros: string,
+        piscina: boolean,
+        pontos: string,
+        price: string,
+        quartos: string,
+        title: string,
+        vagas: string
+    }
 }
